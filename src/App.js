3c53a55ed3a338a3171be131319c2e48705abf2f
Niva Maharjan <109934208+Niva02M@users.noreply.github.com>
2023-07-29 16:43:43 +0545

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Flashcardlist from './flashcardlist';
+import './app.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  const[flashcards, setflashcards]=useState(SAMPLE_FLASHCARDS)
+  
+  useEffect(()=> {
+    axios
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res =>{
+        res.data.results.map
+        console.log(res.data)
+      })
+    }, [])
+  
+  return(
+  <Flashcardlist flashcards={flashcards}/>
   );
 }
+
+const SAMPLE_FLASHCARDS=[
+  {
+    id:1,
+    question:'What is 2+2?',
+    answer:'4',
+    options:[
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id:1,
+    question:'What is 2/2?',
+    answer:'1',
+    options:[
+      '2',
+      '3',
+      '4',
+      '1'
+    ]
+  },
+  {
+    id:1,
+    question:'What is 2+1?',
+    answer:'3',
+    options:[
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  }
+]
 
 export default App;
